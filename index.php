6113dd8376152acdb591bb698c5979def2b76261
coreacathrine <146171629+1120coreacath@users.noreply.github.com>
2024-11-06 23:28:24 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>My first Ubuntu Server PHP Deployment</title>
 </head>
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <body>
 
 	<p>My First Ubuntu Server PHP Deployment</p>
 
-</body>
      <div>
     	<label>First Name:</label>
     	<input type="text" name="firstname"class="form-control"> 
@@ -42,8 +40,7 @@
     	<input type="text" name="firstname"class="form-control">
     </div>
 
-
+</body>
 </html>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
